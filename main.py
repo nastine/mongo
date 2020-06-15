95baa369527f7b8372b38349cac01fc9881506ec
nastine <nastine928@gmail.com>
2020-06-15 13:20:05 +0300

    Datetime added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import re
 from pprint import pprint
 from pymongo import MongoClient
+from datetime import datetime
 
 
 def read_data(csv_file, db):
@@ -9,11 +10,19 @@ def read_data(csv_file, db):
         reader = csv.DictReader(csvfile)
         for line in reader:
             line['Цена'] = int(line['Цена'])
-            collection_artists.insert_one(line)
+            line['Дата']= datetime.strptime(line['Дата']+'.20', '%d.%m.%y')
+            artists.insert_one(line)
 
 
 def find_cheapest(db, collection):
     return list(collection.find().sort('Цена'))
+
+
+def find_date(from_date, to_date, db, collection):
+    result = []
+    for artist in collection.find({"Дата": {"$gte": datetime.strptime(from_date, '%d.%m.%y'), "$lt": datetime.strptime(to_date, '%d.%m.%y')}}).sort('Цена'):
+        result.append(artist)
+    return result
 
 
 def find_by_name(name, db, collection):
@@ -21,13 +30,17 @@ def find_by_name(name, db, collection):
     if symb != None:
         name = name[0:name.find(str(symb))]+'\\'+name[name.find(str(symb)):]
     regx = re.compile(name, re.IGNORECASE)
-    return collection.find_one({"Исполнитель": regx})
-
+    result = []
+    for artist in collection.find({"Исполнитель": regx}).sort('Цена'):
+        result.append(artist)
+    return result
 
 if __name__ == '__main__':
+    
     client = MongoClient()
     tickets_db = client['tickets']
-    collection_artists = tickets_db['artists']
-    # read_data('artists.csv', tickets_db)
-    # pprint(find_cheapest(tickets_db, collection_artists))
-    pprint(find_by_name('T-', tickets_db, collection_artists))
+    artists = tickets_db['artists']
+    read_data('artists.csv', tickets_db)
+    pprint(find_cheapest(tickets_db, artists))
+    pprint(find_by_name('T-', tickets_db, artists))
+    pprint(find_date('01.02.20', '29.02.20', tickets_db, artists))
